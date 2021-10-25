3731db7424ffd96b9c7055f2fcfdd3419cfdf65f
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2021-10-25 17:49:34 +0200

    Add files via upload

---
+++ pr_8_corect.py
@@ -0,0 +1,14 @@
+b=str(input('Dati sirul: '))
+c=b
+d=b
+for i in range(0,len(b)):
+   if ord(b[i]) in range(65,90):
+      c=c.replace(b[i],chr(ord(b[i])+1))
+print(c)
+for i in range(0,len(b)):      
+   if ord(b[i])==90:
+       b=b.replace(b[i],'A')
+   if ord(b[i])==32:
+       d=d.replace(b[i],'-')
+print(b)
+print(d)
