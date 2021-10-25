804a8bc7681d4f8dd0745381b22cedc0f56de943
FedoseevaAlexandra <70970063+FedoseevaAlexandra@users.noreply.github.com>
2021-10-25 17:46:25 +0200

    Add files via upload

--- pr_8.py
+++ pr_8.py
@@ -1,12 +1,14 @@
-
 b='AUPEZH '
+c=b
+d=b
 for i in range(0,len(b)):
    if ord(b[i]) in range(65,90):
-      b=b.replace(b[i],chr(ord(b[i])+1))
+      c=c.replace(b[i],chr(ord(b[i])+1))
+print(c)
 for i in range(0,len(b)):      
    if ord(b[i])==90:
        b=b.replace(b[i],'A')
    if ord(b[i])==32:
-       b=b.replace(b[i],'-')
+       d=d.replace(b[i],'-')
 print(b)
-     
+print(d)
